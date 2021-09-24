a24da1784541d55c4c5f87a849c651ae4c41feb2
John Doe <johndoe@example.com>
2021-09-24 23:11:25 +0400

    jest

--- src/components/Engine/Engine.tsx
+++ src/components/Engine/Engine.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Room from '../Room/Room'
 import map from '../../engine/data/map/map'
+import checkRoom from '../../engine/gameLogic/checkRoom/checkRoom'
 
 const passagesStyle = {
                         display: 'flex',
@@ -12,13 +13,14 @@ const passagesStyle = {
 const Engine: React.FC = () => {
     
     const [heroPosition, setHeroPosition] = useState(() => 0)
+    const [wumpusPosition, setWumpusPosition] = useState(() => 0)
     
     return (
             <>
                 <div style={passagesStyle}>
                 {map[heroPosition].map((room, neighbourIndex)=> <Room goToRoom={setHeroPosition} number={room}/>)}
                 </div>
-                <div>You are at room {heroPosition} there are passages to rooms {map[heroPosition].map(room => `${room} `)}</div>
+                <div>You are at room {heroPosition}, you see {checkRoom([wumpusPosition])(heroPosition)} there are passages to rooms {map[heroPosition].map(room => `${room} `)}</div>
             </>
     )
 }
